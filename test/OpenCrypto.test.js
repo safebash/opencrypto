7a3be37da0cd243955d79b216d473c68464242b6
Peter <peter@safebash.com>
2016-08-31 17:18:06 +0200

    Update OpenCrypto.test.js

--- test/OpenCrypto.test.js
+++ test/OpenCrypto.test.js
@@ -4,12 +4,8 @@ QUnit.test('some basic tests', 7, function (assert) {
 	var x, y;
 
 	assert.equal('Foo', 'Foo', 'Similar strings are.. equal');
-
 	assert.equal(true, 1, 'Boolean true and 1 are similar');
-
 	assert.notStrictEqual(true, 1, '... but, boolean true and 1 are not *strictly* the same');
-
-	// Primitive values are compared by value. Two trues are both stictly the same.
 	assert.strictEqual(true, true, 'of course one boolean true is *strictly* the same as another boolean true');
 
 	x = { one : 1, two: 2 };
@@ -29,28 +25,9 @@ QUnit.test('some basic tests', 7, function (assert) {
 		{ one: 1, two: 2 },
 		'assert.deepEqual compares values, not different references with the same values are equal'
 	);
-
-	// example of failing tests
-
-	/*
-	assert.equal('This string is not the same', 'as this one', 'different strings are.. different, this fails!');
-	*/
-
-	/*
-	assert.equal(
-		{ one : 1, two: 2 },
-		{ one: 1, two: 2 },
-		'assert.equal compares objects by reference, different (but similar) objects are not equal'
-	);
-	*/
 });
 
-QUnit.module('myLib');
-
-QUnit.test('isAwesome', 4, function (assert) {
-	assert.strictEqual(MyLib.isAwesome('hey'), true, 'Strings are awesome');
-	assert.strictEqual(MyLib.isAwesome(123), true, 'Numbers are awesome');
-
-	assert.strictEqual(MyLib.isAwesome(), false, 'Undefined (implicit) is not cool');
-	assert.strictEqual(MyLib.isAwesome(undefined), false, 'Undefined (explicit) is not cool');
+QUnit.module('OpenCrypto');
+QUnit.test('keyFromPassphrase', 4, function (assert) {
+	assert.strictEqual(OpenCrypto.keyFromPassphrase('password', 'uniquesalt', 2048), true, 'Strings are awesome');
 });
