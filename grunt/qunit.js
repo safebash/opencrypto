7d66ec3b890d284b666fcb8602db5fac0ca96b09
Peter <peter@safebash.com>
2017-02-07 02:02:55 +0100

    Update qunit.js

--- grunt/qunit.js
+++ grunt/qunit.js
@@ -2,7 +2,7 @@ module.exports = {
     all: {
         options: {
             urls: [
-                'http://127.0.0.1:8080/tests/tests.html'
+                'http://127.0.0.1:8080/test/index.html'
             ],
             timeout: 10000,
             screenshot: false
