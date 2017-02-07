b136e072ce7b1ee9f2153336456bef8c41036725
Peter <peter@safebash.com>
2017-02-07 02:02:17 +0100

    Create qunit.js

---
+++ grunt/qunit.js
@@ -0,0 +1,11 @@
+module.exports = {
+    all: {
+        options: {
+            urls: [
+                'http://127.0.0.1:8080/tests/tests.html'
+            ],
+            timeout: 10000,
+            screenshot: false
+        }
+    }
+};
